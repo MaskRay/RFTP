851071a912f36cc277f9e728e5b99cb094df93e2
Ray Song <i@maskray.me>
2013-12-15 00:45:11 +0800

    a

--- src/ftp.cc
+++ src/ftp.cc
@@ -44,29 +44,52 @@ int FTP::gets()
       }
       continue;
     case '\r':
-      saved = getc();
-      if (saved != EOF) {
-        if (saved == '\n')
-          brk = true;
-        if (i < MAX_REPLY)
-          reply[i++] = c;
-      }
-      break;
     case '\n':
+      brk = true;
       break;
     default:
-      if (i < MAX_REPLY)
+      if (i < MAX_REPLY-1)
         reply[i++] = c;
       break;
     }
   }
 
+  if (i >= MAX_REPLY-1)
+    err("Reply too long\n");
+  reply[i] = '\0';
+  code = atoi(reply);
   code_family = code / 100;
   return code;
 }
 
+void FTP::clearerr(FILE *stream)
+{
+  if (sock && sock->stream)
+      ::clearerr(sock->stream);
+}
+
 int FTP::read_reply()
 {
+  set_sigaction(SIGALRM, replay_alrm_handler);
+  alarm(ftp->reply_timeout);
+  clearerr(fin);
+
+  int r = gets();
+  if (r == -1) {
+    alarm(0);
+    set_sigaction(SIGALRM, SIG_DFL);
+    err("read_reply failed\n");
+    return -1;
+  }
+
+  print_reply();
+  if (reply[3] == '-') {
+    strncpy(tmp, reply, 3);
+    while (gets() != -1 && (print_reply(), strncmp(tmp, reply, 3)));
+  }
+  alarm(0);
+  set_sigaction(SIGALRM, SIG_DFL);
+  return r;
 }
 
 void FTP::chdir(const char *path)
@@ -77,18 +100,25 @@ void FTP::chdir(const char *path)
 void FTP::cdup(const char *path)
 {
   send_receive("CMD %s", path);
+  return code_family == C_COMPLETION ? pwd(false) : -1;
 }
 
 void FTP::mkdir(const char *path)
 {
   send_receive("MKD %s", path);
+  return code_family == C_COMPLETION ? 0 : -1;
 }
 
-void FTP::help(int argc, char *argv[])
+void FTP::help(const char *arg)
 {
+  if (arg)
+    send_receive("HELP %s", path);
+  else
+    send_receive("HELP");
+  return code_family == C_COMPLETION ? 0 : -1;
 }
 
-void FTP::pwd(int argc, char *argv[])
+void FTP::pwd(bool log)
 {
   send_receive("PWD");
 }
@@ -97,14 +127,23 @@ void FTP::quit(int argc, char *argv[])
 {
 }
 
-void FTP::rhelp(int argc, char *argv[])
+void FTP::rmdir(int argc, char *argv[])
 {
   require_connected();
   require_logged_in();
 }
 
-void FTP::rmdir(int argc, char *argv[])
+ull FTP::size(path)
 {
-  require_connected();
-  require_logged_in();
+  send_receive("SIZE %s", path);
+  if (code == C_NOT_IMPLEMENTED) {
+    has_size_cmd = false;
+    return -1;
+  }
+  if (code != C_COMPLETION)
+    return -1;
+
+  ull res;
+  sscanf(reply, "%*s %llu", &res);
+  return res;
 }
